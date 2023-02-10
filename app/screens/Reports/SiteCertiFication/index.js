ecc6daa1506605c681468c932a535244499d38d6
Akash <akash.posin@gmail.com>
2023-02-10 09:57:46 +0530

    100220203

--- app/screens/Reports/SiteCertiFication/index.js
+++ app/screens/Reports/SiteCertiFication/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SiteCertiFication = () => {
   return (
-    <View>
+    <View style={{width: '100%', height: 100, backgroundColor: 'red'}}>
       <Text>SiteCertiFication</Text>
     </View>
   );
