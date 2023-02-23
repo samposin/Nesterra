f65678a82f041f1c6ab58fe987811e7cd93bfc85
samposin <sam.posin@gmail.com>
2023-02-23 10:31:35 +0530

    23022023

--- app/screens/Reports/AssetsAndExpenses/Components/ActiveSiteCount/index.js
+++ app/screens/Reports/AssetsAndExpenses/Components/ActiveSiteCount/index.js
@@ -1,60 +1,151 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import Svg, {Polyline, g} from 'react-native-svg';
 import {
-  VictoryBar,
-  VictoryTheme,
-  VictoryAxis,
-  VictoryChart,
-  Background,
-  VictoryGroup,
-  VictoryLine,
-  VictoryLabel,
-  VictoryPie,
-  VictoryTooltip,
-} from 'victory-native';
-const CustomLabel = ({props}) => {
-  console.log(props);
+  StyleSheet,
+  Text,
+  Dimensions,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+
+import {Svg} from 'react-native-svg';
+import {useSelector} from 'react-redux';
+const {width, height} = Dimensions.get('screen');
+import {VictoryPie, VictoryLabel} from 'victory-native';
+import Item from '../Item';
+import ItemBold from '../Item/ItemBold';
+import ItemHeaderBold from '../Item/ItemHeaderBold';
+import CountView from '../CountView';
+
+const ActiveSiteCount = ({HeaderText, colours}) => {
+  const {SiteAnlysis} = useSelector(state => state.SiteAnlysis);
+  const {circuitCountPie} = useSelector(state => state.SiteAnlysis);
+  // console.log(SiteAnlysis.inv_site_count, 'circuitCountPie');
+  // console.log(circuitCountPie, 'circuitCountPie');
+  const [category, setCategy] = useState(null);
+  // console.log(HeaderText, colours);
+
   return (
-    <Svg>
-      <VictoryLabel {...props} />
-      <VictoryTooltip
-        {...props}
-        x={150}
-        y={150}
-        orientation="top"
-        pointerLength={0}
-        cornerRadius={50}
-        flyoutWidth={100}
-        flyoutHeight={100}
-        flyoutStyle={{fill: 'black'}}
-      />
-    </Svg>
-  );
-};
-const ActiveSiteCount = () => {
-  return (
-    <View
-      style={{
-        width: 150,
-        height: 150,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <VictoryPie
-        style={{labels: {fill: 'white'}}}
-        innerRadius={50}
-        labelRadius={70}
-        labels={({datum}) => `# ${datum.y}`}
-        labelComponent={<CustomLabel />}
-        data={[
-          {x: 1, y: 5},
-          {x: 2, y: 4},
-          {x: 3, y: 2},
-          {x: 4, y: 3},
-          {x: 5, y: 1},
-        ]}
-      />
+    <View style={{width: '100%', padding: 10}}>
+      <View
+        style={{
+          ...styles.mainView,
+          paddingVertical: 10,
+          marginTop: 10,
+          borderRadius: 15,
+        }}>
+        <View style={{}}>
+          <Text style={styles.upperText}>{HeaderText}</Text>
+        </View>
+        <View style={{...styles.upperView}}>
+          <View style={styles.leftView}>
+            <VictoryPie
+              labels={({datum}) => datum.y}
+              colorScale={[
+                '#097ce6',
+                '#068009',
+                '#d06962',
+                '#feb614',
+                '#0f79e6',
+                '#027f01',
+                '#c7291d',
+              ]}
+              x="LocationType"
+              y="Quantity"
+              radius={({datum}) =>
+                category && category == datum.LocationType ? 80 : 70
+              }
+              innerRadius={30}
+              data={circuitCountPie}
+              events={[
+                {
+                  target: 'data',
+                  eventHandlers: {
+                    onPress: () => {
+                      return [
+                        {
+                          target: 'labels',
+                          mutation: props => {
+                            console.log(
+                              circuitCountPie[props.index].LocationType,
+                            );
+                            let categoryName =
+                              circuitCountPie[props.index].LocationType;
+                            setCategy(categoryName);
+                          },
+                        },
+                      ];
+                    },
+                  },
+                },
+              ]}
+            />
+            <VictoryLabel
+              textAnchor="middle"
+              style={{fontSize: 20, color: 'red'}}
+              x={10}
+              y={120}
+              text="Pie!"
+            />
+          </View>
+          <View style={styles.rightView}>
+            <CountView color="green" title="Atm (2606)" />
+            <CountView color="yellow" title="Branch (2606)" />
+            <CountView color="red" title="Atm (2606)" />
+            <CountView color="red" title="Atm (2606)" />
+            <CountView color="red" title="Atm (2606)" />
+            <CountView color="red" title="Atm (2606)" />
+          </View>
+        </View>
+        <View style={styles.lowerView}>
+          <View
+            style={{
+              width: '100%',
+              height: 35,
+              borderTopColor: '#bababa',
+              borderTopWidth: 0.7,
+              flexDirection: 'row',
+            }}>
+            <Item item1="Location Type" width="20%" />
+            <Item item1="States" width="20%" />
+            <Item item1="Cities" width="20%" />
+            <Item item1="Quantity" width="20%" />
+            <ItemHeaderBold item1="Unique Location" width="20%" />
+          </View>
+          {circuitCountPie &&
+            circuitCountPie.map((item, i) => {
+              return (
+                <TouchableOpacity
+                  onPressIn={() => {
+                    console.log(item.LocationType);
+                    setCategy(item.LocationType);
+                  }}
+                  key={i}
+                  style={{
+                    width: '100%',
+                    height: 35,
+                    borderTopColor: '#bababa',
+
+                    borderTopWidth: 0.7,
+                    flexDirection: 'row',
+                    backgroundColor:
+                      category && category === item.LocationType
+                        ? '#bfffc0'
+                        : null,
+                  }}>
+                  <Item item1={item?.LocationType} width="20%" item={item} />
+                  <Item item1={item?.State} width="20%" item={item} />
+                  <Item item1={item?.City} width="20%" item={item} />
+                  <Item item1={item?.Quantity} width="20%" item={item} />
+                  <ItemBold
+                    item1={item?.UniqueLocCount}
+                    width="20%"
+                    item={item}
+                  />
+                </TouchableOpacity>
+              );
+            })}
+        </View>
+      </View>
     </View>
   );
 };
@@ -62,8 +153,45 @@ const ActiveSiteCount = () => {
 export default ActiveSiteCount;
 
 const styles = StyleSheet.create({
-  borderView: {
-    marginLeft: 10,
-    left: 20,
+  mainView: {
+    width: '100%',
+
+    backgroundColor: '#d1e1d4',
+
+    paddingHorizontal: 5,
+  },
+  upperText: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold',
+    marginLeft: 20,
+  },
+
+  upperView: {
+    width: '100%',
+    height: 180,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    flexDirection: 'row',
+  },
+  leftView: {
+    width: '70%',
+    height: 180,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightView: {
+    width: '30%',
+    height: 180,
+    // backgroundColor: 'green',
+  },
+
+  lowerView: {
+    width: '100%',
+    height: 320,
+    backgroundColor: 'white',
+    marginTop: 20,
   },
 });
--- app/screens/Reports/AssetsAndExpenses/index.js
+++ app/screens/Reports/AssetsAndExpenses/index.js
@@ -4,13 +4,14 @@ import React from 'react';
 import MainCountView from './Components/MainCountView';
 import {color} from 'react-native-reanimated';
 import {useSelector} from 'react-redux';
+import ActiveSiteCount from './Components/ActiveSiteCount';
 
 const AssetsAndExpenses = () => {
   const {circuitCountPie} = useSelector(state => state.SiteAnlysis);
   console.log(circuitCountPie);
   return (
     <>
-      <MainCountView HeaderText="Active Site Count" />
+      <ActiveSiteCount HeaderText="Active Site Count" />
     </>
   );
 };
