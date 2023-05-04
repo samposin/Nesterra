e7117a34c24d77f0b29691350d832a759da02cd7
samposin <sam.posin@gmail.com>
2023-05-04 19:42:27 +0530

    04052023

--- app/screens/AddOrder/Components/Circuits/index.js
+++ app/screens/AddOrder/Components/Circuits/index.js
@@ -7,7 +7,11 @@ import {
   ResetView,
 } from '../CircleView';
 
+import InputTypeView1 from '../InputTypeView/InputTypeView1';
+import {serviceType} from '../../../../utils/addOrders';
+
 const Circuits = () => {
+  const [text, setText] = React.useState('');
   return (
     <>
       <View style={styles.stepView}>
@@ -23,6 +27,42 @@ const Circuits = () => {
         </View>
       </View>
       <ResetView />
+      <View style={styles.itemWraper}>
+        <View style={styles.item}>
+          <InputTypeView1
+            title="Order Type"
+            title2="Reason for Order"
+            data={serviceType}
+          />
+        </View>
+        <View style={styles.item}>
+          <InputTypeView1
+            title="Order Type"
+            title2="Reason for Order"
+            data={serviceType}
+          />
+        </View>
+      </View>
+      {/* ================date picker=================== */}
+      <View style={styles.itemWraper}>
+        <View style={styles.item1}>
+          <View style={styles.DateView}>
+            <Text
+              style={{
+                position: 'absolute',
+                top: -10,
+                left: 10,
+                backgroundColor: '#f2f2f2',
+                paddingHorizontal: 5,
+              }}>
+              Date Created
+            </Text>
+            <Text>06/04/23</Text>
+          </View>
+        </View>
+        <View style={styles.item}></View>
+      </View>
+      {/* ================date picker=================== */}
     </>
   );
 };
@@ -42,5 +82,33 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     flexDirection: 'row',
+  },
+  itemWraper: {
+    width: '100%',
+    height: 45,
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    marginVertical: 8,
+  },
+  item: {
+    width: '48%',
+    height: 45,
+  },
+  item1: {
+    width: '48%',
+    height: 40,
+  },
+  DateView: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#727272',
+    position: 'relative',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 5,
   },
 });
