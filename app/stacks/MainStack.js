a67327e9645908888197523decfec757027dafc2
samposin <sam.posin@gmail.com>
2023-05-22 19:24:19 +0530

    22052023

--- app/stacks/MainStack.js
+++ app/stacks/MainStack.js
@@ -70,7 +70,7 @@ const MainStack = () => {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Login" component={Login} /> */}
         <Stack.Screen name="TabNaV" component={TabNaV} />
 
         <Stack.Screen name="Stats" component={Stats} />
